95d13da39134d4683331ef5e86019d8403a35ae4
Rodrigo Aizpuru <rodrigoaiz@gmail.com>
2026-03-17 20:35:10 -0600

    Add sitemap dependency and create robots.txt for SEO optimization

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
+import sitemap from '@astrojs/sitemap';
 
 import icon from 'astro-icon';
 
@@ -18,6 +19,7 @@ export default defineConfig({
       rehypePlugins: [rehypeKatex],
     }), 
     react(), 
+    sitemap(),
     icon({
       include: {
         bi: [
